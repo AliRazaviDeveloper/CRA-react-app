8f7e141672cedfb9cef53882a427a5384028d0d7
AliRazavi <razavi.ali.1998@gmail.com>
2023-01-24 04:52:52 +0330

    change: ui CRA started

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import React from "react";
+import "./app.css";
 const App = () => {
-  return <h1>Hello World ! {process.env.NODE_ENV}</h1>;
+  const [counter, setCounter] = React.useState(0);
+  return (
+    <div className="container">
+      <h1>Hello Welcome to my custom config react CRA 🚀</h1>
+
+      <p>Counter is : {counter}</p>
+
+      <div className="wrapper--button">
+        <button
+          className="increment"
+          onClick={() => setCounter((counter) => counter + 1)}
+        >
+          INCREMENT
+        </button>
+        <button
+          className="decrement"
+          onClick={() => setCounter((counter) => counter - 1)}
+        >
+          DECREMENT
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
+import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
