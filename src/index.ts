82fa8992e2213f459f3b2e1adf47d156f8a26ae5
AliRazavi <razavi.ali.1998@gmail.com>
2023-01-24 12:04:15 +0330

    fix: ts type element root

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { createRoot } from "react-dom/client";
-import React from "react";
-import App from "./App.jsx";
-import "./index.css";
-const container = document.getElementById("root");
-const root = createRoot(container);
+import { createRoot } from 'react-dom/client'
+import React from 'react'
+import App from './App.jsx'
+import './index.css'
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
 
-root.render(<App />);
+root.render(<App />)
