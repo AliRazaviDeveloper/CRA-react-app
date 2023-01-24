aaa65739f4f4293118670633090537f746920de6
AliRazavi <razavi.ali.1998@gmail.com>
2023-01-24 04:52:41 +0330

    feat: reload react refresh development

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -1,4 +1,10 @@
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
+  devServer: {
+    hot: true,
+  },
+  plugins: [new ReactRefreshWebpackPlugin()],
 };
